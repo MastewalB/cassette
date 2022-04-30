b880e32a4c99696e95590eed2806500085ac3d18
MastewalB <se.mastewal.belete@gmail.com>
2022-05-01 00:38:26 +0300

    episode container

---
+++ lib/src/common_widgets/podcast_episode_small.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class PodcastEpisodeSmall extends StatelessWidget {
+  const PodcastEpisodeSmall({Key? key,
+  required this.children
+  }) : super(key: key);
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
