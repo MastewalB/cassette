2d5b671c6a42aeb1f463c97d7ddd99303501bbed
MastewalB <se.mastewal.belete@gmail.com>
2022-05-16 23:54:40 +0300

    Download Page Starter

---
+++ lib/src/features/download/presentation/download_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:cassette/src/features/download/presentation/widgets/container.dart';
+
+class DownloadPage extends StatefulWidget {
+  const DownloadPage({Key? key}) : super(key: key);
+
+  @override
+  State<DownloadPage> createState() => _DownloadPageState();
+}
+
+class _DownloadPageState extends State<DownloadPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
