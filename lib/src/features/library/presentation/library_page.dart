94ede14d4674a7ef2082f4651093995903f6ce96
MastewalB <se.mastewal.belete@gmail.com>
2022-05-24 04:24:33 +0300

    Library Page

--- lib/src/features/library/presentation/library_page.dart
+++ lib/src/features/library/presentation/library_page.dart
@@ -1,5 +1,6 @@
+import 'package:cassette/src/features/download/presentation/download_page.dart';
 import 'package:flutter/material.dart';
-
+import 'package:cassette/src/common_widgets/common_widgets.dart';
 
 class LibraryPage extends StatefulWidget {
   const LibraryPage({Key? key}) : super(key: key);
@@ -9,30 +10,106 @@ class LibraryPage extends StatefulWidget {
 }
 
 class _LibraryPageState extends State<LibraryPage> {
-  final TrackingScrollController _trackingScrollController = TrackingScrollController();
+  final TrackingScrollController _trackingScrollController =
+      TrackingScrollController();
 
   @override
-  void dispose(){
+  void dispose() {
     _trackingScrollController.dispose();
     super.dispose();
-
   }
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      controller: _trackingScrollController,
-      slivers: [
-        SliverAppBar(
-          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-          title: Text(
-            'Your Library',
-            style: Theme.of(context).textTheme.displayLarge,
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        body: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverOverlapAbsorber(
+                handle:
+                    NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+                sliver: SliverAppBar(
+                  title: Text(
+                    "Your Library",
+                    style: Theme.of(context).appBarTheme.titleTextStyle,
+                  ),
+                  backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                  forceElevated: innerBoxIsScrolled,
+                  automaticallyImplyLeading: false,
+                  expandedHeight: 15.0,
+                  flexibleSpace: FlexibleSpaceBar(),
+                  bottom: TabBar(
+                    indicatorColor: Theme.of(context).indicatorColor,
+                    indicatorWeight: 04,
+                    // indicatorSize: ,
+                    tabs: const [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Downloads",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "History",
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Subscriptions",
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ];
+          },
+          body: TabBarView(
+            children: [
+              DownloadPage(),
+              // Text("Downloads"),
+              Text("History"),
+              Text("Subscriptions"),
+            ],
           ),
-          centerTitle: false,
-          floating: true,
-        )
-      ],
+        ),
+      ),
     );
   }
 }
+//
+// SliverAppBar(
+// backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+// title: Text(
+// 'Your Library',
+// style: Theme.of(context).textTheme.displayLarge,
+// ),
+// centerTitle: false,
+// floating: true,
+// actions: [
+// CircleButton(
+// icon: Icons.search,
+// iconSize: 27.0,
+// onPressed: () => {},
+// )
+// ],
+// bottom: const TabBar(
+// tabs: [
+// Tab(text: "Downloads"),
+// Tab(text: "History"),
+// Tab(text: "Subscriptions"),
+// ],
+// ),
+// ),
