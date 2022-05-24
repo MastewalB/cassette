b2bd84950a198d67db4baf016064e0e4eeab8447
MastewalB <se.mastewal.belete@gmail.com>
2022-05-24 04:23:55 +0300

    Custom Themes

--- lib/src/themes/custom_theme.dart
+++ lib/src/themes/custom_theme.dart
@@ -6,14 +6,29 @@ import 'colors/colors.dart';
 class CustomTheme{
   static ThemeData get lightTheme{
     return ThemeData(
-      primaryColor: Colors.purple,
+      primaryColor: Colors.purple.shade400,
       scaffoldBackgroundColor: Colors.purple.shade600,
+      canvasColor: Colors.purple.shade200,
+      indicatorColor: Colors.purple.shade50,
+      primaryIconTheme: const IconThemeData(
+        color: Colors.white,
+        opacity: 1.0,
+        size: 6.0,
+      ),
       textTheme: const TextTheme(
         displayLarge: TextStyle(
           color: Colors.white,
           fontSize: 24.0,
           fontWeight: FontWeight.bold,
           letterSpacing: -1.0,
+        )
+      ),
+      appBarTheme: const AppBarTheme(
+        centerTitle: true,
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize:28,
+          fontWeight: FontWeight.w400,
         )
       ),
       fontFamily: 'Montserrat',
