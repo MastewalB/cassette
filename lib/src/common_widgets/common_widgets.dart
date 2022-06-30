2eaf9ec0f38481e70a7cca778deca83c60902eb6
MastewalB <se.mastewal.belete@gmail.com>
2022-06-30 16:27:27 +0300

    Podcast Card Small

--- lib/src/common_widgets/common_widgets.dart
+++ lib/src/common_widgets/common_widgets.dart
@@ -1,3 +1,4 @@
 export 'circle_button.dart';
 export 'container.dart';
 export 'pod_card.dart';
+export 'pod_card_small.dart';
---
+++ lib/src/common_widgets/pod_card_small.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class PodCardSmall extends StatelessWidget {
+  final String title;
+  final String duration;
+  final String image;
+
+  const PodCardSmall(
+      {Key? key,
+      required this.title,
+      required this.duration,
+      required this.image})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print("object");
+      },
+      child: Container(
+        // padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 31, 29, 43),
+        ),
+
+        height: 50,
+        alignment: Alignment.center,
+        child: Row(
+          children: [
+            Container(
+              width: 50,
+              height: 50,
+              child: ClipRRect(
+                child: Image.asset(
+                  image,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Text(
+                    title,
+                    style: TextStyle(color: Colors.white, fontSize: 19),
+                    maxLines: 2,
+                  ),
+                ),
+                Flexible(
+                  child: Text(
+                    duration,
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
