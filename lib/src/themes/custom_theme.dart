8504d9b299e86d0ea9500bc8052ea8b1749a3296
MastewalB <se.mastewal.belete@gmail.com>
2022-06-30 11:55:08 +0300

    Custom Theme

--- lib/src/themes/custom_theme.dart
+++ lib/src/themes/custom_theme.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'colors/colors.dart';
 
-
-
-class CustomTheme{
-  static ThemeData get lightTheme{
+class CustomTheme {
+  static ThemeData get lightTheme {
     return ThemeData(
       primaryColor: Colors.purple.shade400,
-      scaffoldBackgroundColor: Colors.purple.shade600,
+      scaffoldBackgroundColor: Color.fromARGB(255, 31, 29, 43),
       canvasColor: Colors.purple.shade200,
       indicatorColor: Colors.purple.shade50,
       primaryIconTheme: const IconThemeData(
@@ -16,26 +14,49 @@ class CustomTheme{
         size: 6.0,
       ),
       textTheme: const TextTheme(
-        displayLarge: TextStyle(
-          color: Colors.white,
-          fontSize: 24.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: -1.0,
-        )
-      ),
+          displayLarge: TextStyle(
+        color: Colors.white,
+        fontSize: 24.0,
+        fontWeight: FontWeight.bold,
+        letterSpacing: -1.0,
+      )),
       appBarTheme: const AppBarTheme(
-        centerTitle: true,
-        titleTextStyle: TextStyle(
-          color: Colors.white,
-          fontSize:28,
-          fontWeight: FontWeight.w400,
-        )
-      ),
+          centerTitle: true,
+          color: Color.fromARGB(255, 31, 29, 43),
+          toolbarHeight: 65,
+          elevation: 10,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+            fontWeight: FontWeight.w400,
+          )),
       fontFamily: 'Montserrat',
       buttonTheme: ButtonThemeData(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0)),
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0)),
         buttonColor: const Color.fromARGB(200, 93, 84, 143),
-      )
+      ),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        landscapeLayout: BottomNavigationBarLandscapeLayout.centered,
+        backgroundColor: Color.fromARGB(255, 47, 49, 66),
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white70,
+
+        selectedIconTheme: IconThemeData(
+          size: 25
+        ),
+        unselectedIconTheme: IconThemeData(
+          size: 22,
+        ),
+
+        selectedLabelStyle: TextStyle(
+          fontFamily: 'Montserrat',
+          fontSize: 15,
+        ),
+        unselectedLabelStyle: TextStyle(
+          fontSize: 14,
+        ),
+      ),
     );
   }
 }
