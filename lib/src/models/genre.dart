b475750df02101278461c9aedc8f866b31b189cb
MastewalB <se.mastewal.belete@gmail.com>
2022-06-30 11:54:58 +0300

    models

---
+++ lib/src/models/genre.dart
@@ -0,0 +1,11 @@
+class Genre {
+  const Genre({
+    required this.id,
+    required this.name,
+    required this.podcastCount,
+  });
+
+  final int id;
+  final String name;
+  final int podcastCount;
+}
---
+++ lib/src/models/podcast.dart
@@ -0,0 +1,13 @@
+class Podcast {
+  final String title;
+  final String description;
+  final String author;
+  final String image;
+
+  const Podcast({
+    required this.description,
+    required this.author,
+    required this.image,
+    required this.title,
+  });
+}
